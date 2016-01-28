b34d35c1387d5000c4c9e45379e91e9889a5a0c3
unknown <lcx061125@163.com>
2016-01-28 20:13:25 +0800

    transport from git.oschina.net

--- Source/copy.bat
+++ Source/copy.bat
@@ -1,6 +1,6 @@
 echo "Enter What You Want:"
-copy /Y D:\workspace\BC\DISK_C\BORLANDC\APP\*.h C:\Users\Clay\Documents\github\RU\Source\
-copy /Y D:\workspace\BC\DISK_C\BORLANDC\APP\*.cpp C:\Users\Clay\Documents\github\RU\Source\
-copy /Y D:\workspace\BC\DISK_C\BORLANDC\APP\MAIN.EXE C:\Users\Clay\Documents\github\RU\Source\RU.EXE
+copy /Y D:\workspace\BC\DISK_C\BORLANDC\APP\*.h C:\Users\Clay\Documents\github\DOSTOOL\Source
+copy /Y D:\workspace\BC\DISK_C\BORLANDC\APP\*.cpp C:\Users\Clay\Documents\github\DOSTOOL\Source
+copy /Y D:\workspace\BC\DISK_C\BORLANDC\APP\MAIN.EXE C:\Users\Clay\Documents\github\DOSTOOL\Source\DOSTOOL.EXE
 
 
